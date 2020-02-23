2316f47c270755d9e8fe5d8e8da4f27a6c83d605
jankenshow <perchon001@gmail.com>
2020-02-24 01:12:12 +0900

    add comment on __init__.py

--- c_module/hello_package/hello_package/__init__.py
+++ c_module/hello_package/hello_package/__init__.py
@@ -1,3 +1,6 @@
 from hello_package.hello_module import *
 from hello_package.hello_module_copy import *
 # from hello_package.copy.hello_module_copy import *
+
+### 書き方変えないと `from hello_package import *` で
+### ２つのモジュール内の関数がそのまま使えてしまう。。
